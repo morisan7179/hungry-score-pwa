db33b5a158ddefd99b2d8f21aa55f46c68fc3023
morisan7179 <koi-jiro@nifty.com>
2025-05-07 20:10:55 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
-import React from 'react'; export default function App() { return <div>空腹スコア・アプリ</div>; }
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
