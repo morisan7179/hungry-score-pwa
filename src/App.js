b8a7c1dbb0272ab82706333dc03943d74ecc4a5d
morisan7179 <koi-jiro@nifty.com>
2025-05-07 20:41:54 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
-import React, { useState, useEffect } from 'react';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import ProgressPage from './ProgressPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/
+
 
 function App() {
   const [score, setScore] = useState('');
